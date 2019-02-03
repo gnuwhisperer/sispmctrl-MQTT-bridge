d601c902c09d49a436d23deabe2754794ad67ffc
Martin Sofaru <ms@cloud-related.de>
2019-02-03 12:46:25 +0100

    migrate to paho mqtt, add state reports, make mqtt messages similar to tasmota

--- README.md
+++ README.md
@@ -13,16 +13,19 @@ How to use:
 
 The command 
 
->mosquitto_pub -h brokerIP -t "sispmcltr/DEVICE_SERIAL/OUTLET/set" -m On
+>mosquitto_pub -h brokerIP -t "cmnd/sispmctl/<device_id>/POWER<outlet>" -m On
 
-Will turn on outlet OUTLET (numbered 1-4) on the gembird device with the serial DEVICE_SERIAL. 
+Will turn on outlet <outlet> (numbered 1-4) on the gembird device with the serial <device_id>. 
 
 To get the ids run: 
 >sispmctl -s  
 
 The script will also write a 
 
-sispmcltr/01:1c:aa:b5:41/connected True
+tele/sispmctl/01:1c:aa:b5:41/LWT True
 
-When a device is detected or False when disconnected. 
+when a device is detected or False when disconnected. 
 
+The script will send the changed state of the outlets in POLL_TIME = 5 intervals or after switching:
+
+tele/sispmctl/01:1c:aa:b5:41/STATE = {"POWER1":"OFF", "POWER2":"OFF", "POWER3":"OFF", "POWER4":"OFF"}
--- sispmctl-to-MQTT.py
+++ sispmctl-to-MQTT.py
@@ -1,177 +1,183 @@
 #!/usr/bin/python
 
-import mosquitto,sys
+import ConfigParser
 import json
+import os
+import sys
 import thread
 import time
-import sys
-import os
-import ConfigParser
 
-#The MQTT format for setting outlets are PREFIX/DEVICE_ID/OUTLET/set 
+import paho.mqtt.client as mqtt
 
-POLL_TIME = 5 #For checking state changes on outlets. 
+# The MQTT format for setting outlets are cmnd/<prefix>/<device_id>/POWER<outlet>
+POLL_TIME = 5  # For checking state changes on outlets.
+QOS = 1
 
+def socket_off(device, socket):
+	global states
+	#print("turn socket", socket, "off")
+	os.system("/usr/bin/sispmctl -q -D %s -f %i" % (device, socket))
+	states[device]=socket_get_state(device)
+	publish_state(device)
 
-def socket_off(device,socket):
-    #print "turn socket",num,"off"
-    #print "/usr/local/bin/sispmctl -f "+str(num)
-    os.system("/usr/bin/sispmctl -q -d %i -f %i" %(device,socket))
-
-def socket_on(device,socket):
-    #print "turn socket",num,"on"
-    os.system("/usr/bin/sispmctl -q -d %i -o %i" %(device,socket))
-    
-
-def socket_get_state(num):
-    #print "get state of socket",num
-    #fd=os.popen("/usr/local/bin/sispmctl -g "+str(num))
-    result=[]
-    for line in os.popen("/usr/bin/sispmctl -q -d %s -n -g all" % str(num)).readlines():
-        #print "line: ",line       
-        if line[0]=="0":
-            result.append(0)
-        elif line[0]=="1":
-            result.append(1)
+def socket_on(device, socket):
+	global states
+	#print("turn socket", socket, "on")
+	os.system("/usr/bin/sispmctl -q -D %s -o %i" % (device, socket))
+	states[device]=socket_get_state(device)
+	publish_state(device)
+	
+def socket_get_state(device):
+    #print("get state of all socket",device)
+    result = {}
+    for line in os.popen("/usr/bin/sispmctl -q -D %s -n -g all" % device).readlines():
+        if line[0] == "0":
+            result["POWER"+str(len(result)+1)]="OFF"
+        elif line[0] == "1":
+            result["POWER"+str(len(result)+1)]="ON"
     return result
-    
-def socket_get_serialnumbers():
-    #print "get state of socket",num
-    #fd=os.popen("/usr/local/bin/sispmctl -g "+str(num))
-    Devices = {}
-    for line in os.popen("/usr/bin/sispmctl -s ").readlines():
-        #print "line: ",line       
-        if line.find("serial number") != -1:
-            Serial = line.strip("\n").split("    ")[1]
-            Devices[Serial]=len(Devices)
-            
-    return Devices
 
-def on_connect(mosq, rc,a):
-    mosq.subscribe(prefix+"/#", 0)
+def devices_get_serialnumbers():
+	#generate device list: {'01:02:03:04:05': 0}
+	discovered_devices = {}
+	discovered_states = {}
+	for line in os.popen("/usr/bin/sispmctl -s ").readlines():
+		if line.find("serial number") != -1:
+			serial = line.strip("\n").split("    ")[1]
+			discovered_devices[serial] = len(discovered_devices)
+			discovered_states[serial] = socket_get_state(serial)
+	return (discovered_devices,discovered_states)
 
-def on_message(a,mosq, msg):
-    global devices
-    #try:
-    if True:
-    	print("RECIEVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload))
-    	topics = msg.topic.split("/")
-    	name = topics[-2]
-    	if topics[-1] == "set":
-    	    value = msg.payload
-    	    if not topics[-3] in devices:
-    	        return
+def publish_state(device):
+	global states
+	topic = "tele/"+ prefix +"/"+ device +"/STATE"
+	#message = str(states[device])
+	message = json.dumps(states[device], sort_keys=True)
+	client.publish(topic, message , QOS)
+	print("SENT MQTT MESSAGE: "+topic+ " " + message)
 
-    	    #print topics 
-	    #print value
- 
-    	    device = devices[topics[-3]]
-    	    socket = int(topics[-2])
-    	    
-    	    if value.upper()== "ON" or value == "1":
-    	        #Turn on
-    	        socket_on(device,socket)
-    	    else:
-    	        #Turn off
-    	        socket_off(device,socket)
-    	    
-    #except:
+def on_connect(mqtt, rc, a):
+    mqtt.subscribe("cmnd/"+prefix+"/#", 0)
+
+def on_message(a, mqtt, msg):
+	global devices
+	# try:
+	if True:
+		print("RECEIVED MQTT MESSAGE: "+msg.topic + " " + str(msg.payload))
+		#cmnd/<prefix>/<serial>/POWER<socket+1> = <ON|OFF|0|1>
+		topics = msg.topic.split("/")
+		socket = int(topics[-1][-1])
+		#print("socket: "+str(socket))
+		value = msg.payload
+		device = topics[-2]
+		if not device in devices:
+			return
+		if value.upper() == "ON" or value == "1":
+			# Turn on
+			socket_on(device, socket)
+		else:
+		    # Turn off
+		    socket_off(device, socket)
+
+    # except:
     #   print "Error occured while processing command"
-    return 
-    
+	return
+
+
 def ControlLoop():
     # schedule the client loop to handle messages, etc.
-    print "Starting MQTT listener"
+    print("Starting MQTT listener")
     while True:
         client.loop()
         time.sleep(0.1)
 
 
-#Get all devices.
+# Get all devices.
 if __name__ == '__main__':
-
-
-	#Where am I 
+	# Where am I
 	path = os.path.abspath(os.path.dirname(sys.argv[0]))
-
-	#Load config file... 	
-
+	# Load config file...
 	try:
 		ConfigFile = sys.argv[1]
 	except:
-		ConfigFile = path + "/sispmctrl.conf"
+		ConfigFile = path + "/sispmctl.conf"
 
 	try:
-		f = open(ConfigFile,"r")
+		f = open(ConfigFile, "r")
 		f.close()
 	except:
 		try:
-			ConfigFile = path + "/sispmctrl.conf"
-			f = open(ConfigFile,"r")
-                	f.close()
+			ConfigFile = path + "/sispmctl.conf"
+			f = open(ConfigFile, "r")
+			f.close()
 		except:
-			print "Please provide a valid config file! By argument or as default sispmctrl.conf file."
+			print("Please provide a valid config file! By argument or as default sispmctl.conf file.")
 			exit(1)
 	config = ConfigParser.RawConfigParser(allow_no_value=True)
 	config.read(ConfigFile)
 
-
-	#Load basic config. 
-	ip = config.get("MQTTServer","Address")
-	port = config.get("MQTTServer","Port")
-	user = config.get("MQTTServer","User")
-	password = config.get("MQTTServer","Password")
-	prefix = config.get("MQTTServer","Prefix")   
-
-
-	client = mosquitto.Mosquitto("sispmctl-to-MQTT-client")
+	# Load basic config.
+	ip = config.get("MQTTServer", "Address")
+	port = config.get("MQTTServer", "Port")
+	user = config.get("MQTTServer", "User")
+	password = config.get("MQTTServer", "Password")
+	prefix = config.get("MQTTServer", "Prefix")
+	instance = config.get("MQTTServer", "Instance")
+	client = mqtt.Client("sispmctl-to-MQTT-client")
 
 	if user != None:
-    			client.username_pw_set(user,password)
+    			client.username_pw_set(user, password)
 
-	client.will_set( topic =  "system/" + prefix, payload="Offline", qos=1, retain=True)
+	client.will_set(topic="tele/" + prefix+"/"+instance+"/LWT",
+	                payload="Offline", qos=1, retain=True)
 
-	#Connect and notify others of our presence. 
+	# Connect and notify others of our presence.
 	client.connect(ip)
-	client.publish("system/" + prefix, "Online",1, retain=True)
+	client.publish("tele/"+prefix+"/"+instance+"/LWT", "Online", 1, retain=True)
 	client.on_connect = on_connect
 	client.on_message = on_message
-	client.subscribe(prefix+"/#", 0)
+	client.subscribe("cmnd/"+prefix+"/#", 0)
 
-	#Init
-	
+	# Init
 	devices = {}
 	states = {}
-	
-	#Start tread...
-	thread.start_new_thread(ControlLoop,())
-	
+
+	# Start tread...
+	thread.start_new_thread(ControlLoop, ())
+
 	while True:
-	   #Check if anything changed
-	   #Send update if it did. 
-	    
-	    #Detect devices
-	    old_devices = devices
-	    devices = socket_get_serialnumbers()
-	    
-	    #Look for disconnections..
-	    for device in old_devices:
-	        if not device in devices:
-	            topic = prefix +"/"+ device + "/connected"
-	            client.publish(topic , False, 1)
+		# Check if anything changed
+		# Send update if it did.
+		# Detect devices
+		old_devices = devices
+		old_states = states
+		devices,states = devices_get_serialnumbers()
+		# Look for disconnections..
+		for device in old_devices:
+			if not device in devices:
+				print("device disconnected: "+device)
+				topic = "tele/"+prefix +"/"+ device + "/LWT"
+				client.publish(topic , False, QOS)
+
+		# Look for devices being connected..
+		for device in devices:
+			if not device in old_devices:
+				print("device connected: "+device)
+				topic = "tele/"+prefix +"/"+ device + "/LWT"
+				client.publish(topic , True, QOS)
 	            
-	    #Look for devices being connected..
-	    for device in devices:
-	        if not device in old_devices:
-	            topic = prefix +"/"+ device + "/connected"
-	            client.publish(topic , True, 1)
-	            
-	    #Detect states changes on outlets
-	    #for device in devices:
-	        
-	    
-	    time.sleep(POLL_TIME)    
-	       
-	    
+		# Detect states changes on outlets
+		#print(devices)states[device] != old_states[device]:
+		for device in devices:
+			if not device in old_states:
+				#no old state, publish state
+				publish_state(device)
+			else:
+				# old state exists, only publish if changed
+				# compare stateclient.publish(topic, str(states[device]), QOS)
+				#print("newstate: "+str(states[device])+" oldstate: "+str(old_states[device]))
+				if states[device] != old_states[device]:
+					publish_state(device)
+		time.sleep(POLL_TIME)    
 	    
 	client.disconnect() 
